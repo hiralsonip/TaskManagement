52a0d27ba7abfc6d99e1587f806db096f986e6fb
hiralsonip <66827672+hiralsonip@users.noreply.github.com>
2025-09-20 12:45:46 -0400

    Login and Signup - Bcrypt

--- apps/api/src/app/auth/auth.service.ts
+++ apps/api/src/app/auth/auth.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserService } from '../user/user.service';
+import bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    constructor(private userService: UserService) { }
+
+    async loginUser(username: string, password: string): Promise<any> {
+        const user = await this.userService.findOne(username);
+
+        if (!user) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+
+        const { password: pwd, ...result } = user;
+        return result;
+    }
+
+    async registerUser(username: string, password: string): Promise<any> {
+        const existingUser = await this.userService.findOne(username);
+        if (existingUser) {
+            throw new UnauthorizedException('Username already exists');
+        }
+
+        const newUser = await this.userService.create({ username, password });
+        const { password: pwd, ...result } = newUser;
+        return result;
+    }
+}
--- apps/api/src/app/user/user.service.ts
+++ apps/api/src/app/user/user.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '../entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import bcrypt from 'bcrypt';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+    constructor(
+        @InjectRepository(User)
+        private userRepository: Repository<User>
+    ) { }
+
+    async create({ username, password }: { username: string; password: string }): Promise<User> {
+        const hashedPassword = await bcrypt.hash(password, 10); // Hash the password
+        const userWithHashedPassword = { username, password: hashedPassword };
+        const newUser = this.userRepository.create(userWithHashedPassword);
+        return this.userRepository.save(newUser);
+    }
+
+    async findOne(username: string): Promise<User | undefined> {
+        return this.userRepository.findOne({ where: { username } });
+    }
+}
