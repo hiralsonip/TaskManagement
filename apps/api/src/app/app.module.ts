2f978b41a48f0780f3a6086882227a6f66f0422f
hiralsonip <66827672+hiralsonip@users.noreply.github.com>
2025-09-20 07:49:21 -0400

    Project Setup

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { RolesModule } from './roles/roles.module';
+import { TasksModule } from './tasks/tasks.module';
+import { OrganizationService } from './organization/organization.service';
+import { OrganizationModule } from './organization/organization.module';
+import { AuditLogController } from './audit-log/audit-log.controller';
+import { AuditLogModule } from './audit-log/audit-log.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    AuthModule,
+    UserModule,
+    RolesModule,
+    TasksModule,
+    OrganizationModule,
+    AuditLogModule,
+  ],
+  controllers: [AppController, AuditLogController],
+  providers: [AppService, OrganizationService],
 })
 export class AppModule {}
