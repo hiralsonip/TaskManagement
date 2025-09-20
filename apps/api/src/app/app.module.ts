c5512441cb501c29fe0d07157aabc40a161ecd66
hiralsonip <66827672+hiralsonip@users.noreply.github.com>
2025-09-20 08:12:01 -0400

    Database setup

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -9,9 +10,17 @@ import { OrganizationService } from './organization/organization.service';
 import { OrganizationModule } from './organization/organization.module';
 import { AuditLogController } from './audit-log/audit-log.controller';
 import { AuditLogModule } from './audit-log/audit-log.module';
+import { User } from './entities/user.entity';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'apps/api/taskmanagement.sqlite',
+      entities: [User],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([User]),
     AuthModule,
     UserModule,
     RolesModule,
@@ -22,4 +31,4 @@ import { AuditLogModule } from './audit-log/audit-log.module';
   controllers: [AppController, AuditLogController],
   providers: [AppService, OrganizationService],
 })
-export class AppModule {}
+export class AppModule { }
